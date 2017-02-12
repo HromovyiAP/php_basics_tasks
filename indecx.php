f7657a41bc755a6d18daca248a3d7b3b2839dfb1
Hromovyi Andrii <Hromovyi@i.ua>
2017-02-12 12:54:52 +0200

    first

---
+++ indecx.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Задачі по РНР</title>
+    <meta charset="UTF-8">
+<body>
+<a href="./1.php" title="Задача №1" target="_blank"> Задача №1 </a> <br>
+<a href="./2.php" title="Задача №2" target="_blank"> Задача №2 </a> <br>
+<a href="./3.php" title="Задача №3" target="_blank"> Задача №3 </a> <br>
+<a href="./4.php" title="Задача №4" target="_blank"> Задача №4 </a> <br>
+<a href="./5.php" title="Задача №5" target="_blank"> Задача №5 </a> <br>
+<a href="./6.php" title="Задача №6" target="_blank"> Задача №6 </a> <br>
+
+</body>
+
+
